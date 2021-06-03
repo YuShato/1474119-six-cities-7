c3ad9024bb4f49dd86e0755baf7fb92f84887977
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2021-06-03 23:40:35 +0300

    1.3.3 добаляет генерацию элементов из массива

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import Main from '../main/main';
+import {number, shape, string, arrayOf, bool} from 'prop-types';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App(props) {
+  const {mockCardsData} = props;
+  return (
+    <Main mockCardsData={mockCardsData}/>
+  );
 }
 
+App.propTypes = {
+  mockCardsData: arrayOf(
+    shape({
+      id: number.isRequired,
+      name: string.isRequired,
+      type: string.isRequired,
+      imgPreview: string.isRequired,
+      price: number.isRequired,
+      isPremium: bool.isRequired,
+    }),
+  ),
+};
 export default App;
