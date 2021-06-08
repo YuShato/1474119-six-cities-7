91052032a3e012867084c3bdac5efc2db9139ee1
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2021-06-08 14:04:27 +0300

    3.1.0 Header&Footer вынесены в отдельные компоненты

--- project/src/components/main/main.jsx
+++ project/src/components/main/main.jsx
@@ -1,43 +1,49 @@
 import React from 'react';
 import Card from '../card/card';
 import PropTypes from 'prop-types';
-import Logo from '../logo/logo';
-import LocationItem from '../location-item/location-item';
+import Header from '../header/header';
 
 function Main (props) {
-  const {mockCardsData, cities} = props;
-
+  const {mockCardsData} = props;
   return (
     <div>
       <div className="page page--gray page--main">
-        <header className="header">
-          <div className="container">
-            <div className="header__wrapper">
-              <Logo/>
-              <nav className="header__nav">
-                <ul className="header__nav-list">
-                  <li className="header__nav-item user">
-                    <a className="header__nav-link header__nav-link--profile" href="#">
-                      <div className="header__avatar-wrapper user__avatar-wrapper">
-                      </div>
-                      <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          </div>
-        </header>
-
+        <Header />
         <main className="page__main page__main--index">
           <h1 className="visually-hidden">Cities</h1>
           <div className="tabs">
             <section className="locations container">
               <ul className="locations__list tabs__list">
-                {
-                  cities.map((city) => <LocationItem key = {city.id} name={city.name}/>,
-                  )
-                }
+                <li className="locations__item">
+                  <a className="locations__item-link tabs__item" href="#">
+                    <span>Paris</span>
+                  </a>
+                </li>
+                <li className="locations__item">
+                  <a className="locations__item-link tabs__item" href="#">
+                    <span>Cologne</span>
+                  </a>
+                </li>
+                <li className="locations__item">
+                  <a className="locations__item-link tabs__item" href="#">
+                    <span>Brussels</span>
+                  </a>
+                </li>
+                <li className="locations__item">
+                  <a className="locations__item-link tabs__item tabs__item--active">
+                    <span>Amsterdam</span>
+                  </a>
+                </li>
+                <li className="locations__item">
+                  <a className="locations__item-link tabs__item" href="#">
+                    <span>Hamburg</span>
+                  </a>
+                </li>
+                <li className="locations__item">
+                  <a className="locations__item-link tabs__item" href="#">
+                    <span>Dusseldorf</span>
+                  </a>
+                </li>
               </ul>
             </section>
           </div>
@@ -81,7 +87,6 @@ function Main (props) {
 
 Main.propTypes = {
   mockCardsData: PropTypes.array.isRequired,
-  cities: PropTypes.array.isRequired,
 };
 
 
--- project/src/index.js
+++ project/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
-import mockCardsData from './components/mock-data/mock';
+import mockCardsData from './components/mocks/mock';
 import Cities from './components/consts/cities';
 
 ReactDOM.render(
