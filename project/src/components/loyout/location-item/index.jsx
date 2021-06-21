8b8ef13f3eeb7120fbfd0035f342c103ad3b23f7
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2021-06-21 18:00:05 +0300

    4.2.1 Исправляет ошибку в элементе LocationItem

--- project/src/components/loyout/location-item/index.jsx
+++ project/src/components/loyout/location-item/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function LocationItem (city) {
+function LocationItem ({city}) {
   return (
     <li className="locations__item">
-      <a className={city.name === 'Amsterdam' ? 'locations__item-link tabs__item tabs__item--active' : 'locations__item-link     tabs__item'} href="#">
-        <span>{city.name}</span>
+      <a className={city === 'Amsterdam' ? 'locations__item-link tabs__item tabs__item--active' : 'locations__item-link     tabs__item'} href="#">
+        <span>{city}</span>
       </a>
     </li>
   );
 }
 
+LocationItem.propTypes = {
+  city: PropTypes.string,
+};
 export default LocationItem;
