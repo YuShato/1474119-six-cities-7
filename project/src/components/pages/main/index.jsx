bc3096205ffc609d8a888e9a2254a6f311d710b8
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2021-07-01 01:27:11 +0300

    5.2.1 Выносит PlacesSort в отдельный компонент

--- project/src/components/pages/main/index.jsx
+++ project/src/components/pages/main/index.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
 import PlacesList from '../../loyout/places-list';
+import PlacesSort from '../../loyout/places-sort';
 import Header from '../../loyout/header/header';
 import LocationsTabs from '../../loyout/locations-tabs';
 import Map from '../../loyout/map';
@@ -28,32 +29,7 @@ function MainPage ({onCitySelect, city, offers})  {
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">{offers.length} places to stay in {city}</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width={7} height={4}>
-                    <use xlinkHref="#icon-arrow-select" />
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li
-                    className="places__option places__option--active"
-                    tabIndex={0}
-                  >
-                    Popular
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: low to high
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: high to low
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Top rated first
-                  </li>
-                </ul>
-              </form>
+              <PlacesSort />
               <PlacesList offers={offers}  pageType='main'/>
             </section>
             <div className="cities__right-section">
