555de5ef4091abb674de9a2a1cc7f606fab2b06d
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2021-07-01 16:56:12 +0300

    5.2.4 Выделяет FavoritesEmpty в отдельный компонент

--- project/src/components/pages/favorites/index.jsx
+++ project/src/components/pages/favorites/index.jsx
@@ -2,44 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Footer from '../../loyout/footer';
 import Header from '../../loyout/header/header';
-import PlacesList from '../../loyout/places-list';
-import { getOffersByCity } from '../../../common';
+import FavoritesEmpty from '../../loyout/favorites-empty';
+import FavoritesContainer from '../../loyout/favorites-container';
 
 function FavoritesPage ({offers}) {
   return (
     <div className="page">
       <Header />
-      <main className="page__main page__main--favorites">
-        <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            <ul className="favorites__list">
-              <li className="favorites__locations-items">
-                <div className="favorites__locations locations locations--current">
-                  <div className="locations__item">
-                    <a className="locations__item-link" href="#">
-                      <span>Amsterdam</span>
-                    </a>
-                  </div>
-                </div>
-                {/* Временное решение */}
-                <PlacesList offers={getOffersByCity(offers, 'Amsterdam')} pageType='favorites'/>
-              </li>
-              <li className="favorites__locations-items">
-                <div className="favorites__locations locations locations--current">
-                  <div className="locations__item">
-                    <a className="locations__item-link" href="#">
-                      <span>Cologne</span>
-                    </a>
-                  </div>
-                </div>
-                {/* Временное решение */}
-                <PlacesList offers={getOffersByCity(offers, 'Cologne')} pageType='favorites' />
-              </li>
-            </ul>
-          </section>
-        </div>
-      </main>
+
+      {offers.length ? <FavoritesContainer offers={offers}/> : <FavoritesEmpty/>}
+
       <Footer />
     </div>
   );
