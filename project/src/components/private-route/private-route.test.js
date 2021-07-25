36a94ed63b364b71ca8519e8d9fcbd9254affcee
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2021-07-25 15:23:55 +0300

    Исправляет файл private-route.test.js

--- project/src/components/private-route/private-route.test.js
+++ project/src/components/private-route/private-route.test.js
@@ -1,62 +1,63 @@
-// import React from 'react';
-// import {Router, Route} from 'react-router-dom';
-// import {createMemoryHistory} from 'history';
-// import {render, screen} from '@testing-library/react';
-// import configureStore from 'redux-mock-store';
-// import {Provider} from 'react-redux';
-// import {AuthorizationStatus} from '../../common/const';
-// import PrivateRoute from './private-route';
+import React from 'react';
+import {Router, Route} from 'react-router-dom';
+import {createMemoryHistory} from 'history';
+import {render, screen} from '@testing-library/react';
+import configureStore from 'redux-mock-store';
+import {Provider} from 'react-redux';
+import {AuthorizationStatus} from '../../common/const';
+import PrivateRoute from './private-route';
+import '@testing-library/jest-dom/extend-expect';
 
-// const mockStore = configureStore({});
-// let history;
+const mockStore = configureStore({});
+let history;
 
-// describe('Test PrivateRouter', () => {
-//   beforeEach(() => {
-//     history = createMemoryHistory();
-//     history.push('/private');
-//   });
+describe('Test PrivateRouter', () => {
+  beforeEach(() => {
+    history = createMemoryHistory();
+    history.push('/private');
+  });
 
-//   it('Should be render component for public route, when user not authorized', () => {
-//     const store = mockStore({
-//       USER: {authorizationStatus: AuthorizationStatus.NO_AUTH},
-//     });
+  it('Should be render component for public route, when user not authorized', () => {
+    const store = mockStore({
+      USER: {authorizationStatus: AuthorizationStatus.NO_AUTH},
+    });
 
-//     render(
-//       <Provider store={store}>
-//         <Router history={history}>
-//           <Route exact path="/login"><h1>Public Route</h1></Route>
-//           <PrivateRoute
-//             exact
-//             path="/private"
-//             render={() => (<h1>Private Route</h1>)}
-//           />
-//         </Router>
-//       </Provider>,
-//     );
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <Route exact path="/login"><h1>Public Route</h1></Route>
+          <PrivateRoute
+            exact
+            path="/private"
+            render={() => (<h1>Private Route</h1>)}
+          />
+        </Router>
+      </Provider>,
+    );
 
-//     expect(screen.getByText(/Public Route/i)).toBeInTheDocument();
-//     expect(screen.queryByText(/Private Route/i)).not.toBeInTheDocument();
-//   });
+    expect(screen.getByText(/Public Route/i)).toBeInTheDocument();
+    expect(screen.queryByText(/Private Route/i)).not.toBeInTheDocument();
+  });
 
-//   it('Should be render component for private route, when user authorized', () => {
-//     const store = mockStore({
-//       USER: {authorizationStatus: AuthorizationStatus.AUTH},
-//     });
+  it('Should be render component for private route, when user authorized', () => {
+    const store = mockStore({
+      USER: {authorizationStatus: AuthorizationStatus.AUTH},
+    });
 
-//     render(
-//       <Provider store={store}>
-//         <Router history={history}>
-//           <Route exact path="/login"><h1>Public Route</h1></Route>
-//           <PrivateRoute
-//             exact
-//             path="/private"
-//             render={() => (<h1>Private Route</h1>)}
-//           />
-//         </Router>
-//       </Provider>,
-//     );
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <Route exact path="/login"><h1>Public Route</h1></Route>
+          <PrivateRoute
+            exact
+            path="/private"
+            render={() => (<h1>Private Route</h1>)}
+          />
+        </Router>
+      </Provider>,
+    );
 
-//     expect(screen.getByText(/Private Route/i)).toBeInTheDocument();
-//     expect(screen.queryByText(/Public Route/i)).not.toBeInTheDocument();
-//   });
-// });
+    expect(screen.getByText(/Private Route/i)).toBeInTheDocument();
+    expect(screen.queryByText(/Public Route/i)).not.toBeInTheDocument();
+  });
+});
