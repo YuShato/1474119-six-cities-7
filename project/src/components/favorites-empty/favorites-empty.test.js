be2a4cb2064196e5560f675d548db4cc32ef2e51
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2021-07-25 16:53:52 +0300

    Добавляет тест для favorites-empty

---
+++ project/src/components/favorites-empty/favorites-empty.test.js
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import FavoritesEmpty from './favorites-empty';
+
+it('Render \'FavoritesEmpty\'', () => {
+  render(
+    <FavoritesEmpty />,
+  );
+
+  expect(screen.getByTestId('favorites-empty')).toBeInTheDocument();
+  expect(screen.getByText('Nothing yet saved.')).toBeInTheDocument();
+});
