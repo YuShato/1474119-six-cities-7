3d084e375993bf2f04d3728562d971e934a15ac5
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2021-07-25 15:36:10 +0300

    Добавляет тесты к  файлам review-wrapper, review-list, sorting-list

---
+++ project/src/components/sorting-list/sorting-list.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import configureStore from 'redux-mock-store';
+import * as redux from 'react-redux';
+import SortingList from './sorting-list';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom/extend-expect';
+
+const mockStore = configureStore();
+
+describe('Test \'Sort\'', () => {
+  it('Render \'SortingList\'', () => {
+    render(
+      <redux.Provider store={mockStore({PLACES: {activeSorting: 'Popular'}})}>
+        <SortingList />
+      </redux.Provider>,
+    );
+
+    expect(screen.getByText('Sort by')).toBeInTheDocument();
+    expect(screen.getByTestId('places-sorting')).toBeInTheDocument();
+    expect(screen.getByText('Popular')).toBeInTheDocument();
+  });
+
+  it('Should call dispatch when user click on sort', () => {
+    const fakeDispatch = jest.spyOn(redux, 'useDispatch');
+
+    render(
+      <redux.Provider store={mockStore({PLACES: {activeSorting: ''}})}>
+        <SortingList />
+      </redux.Provider>,
+    );
+
+    userEvent.click(screen.getByTestId('places-sorting-type'));
+    expect(screen.getByTestId('places-options')).toHaveClass('places__options--opened');
+
+    userEvent.click(screen.getByText('Popular'));
+    expect(fakeDispatch).toBeCalled();
+  });
+});
