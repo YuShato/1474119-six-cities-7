663237193ce0a0055f8fc7b3aa6e8d7d041e6ee6
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2021-08-01 06:57:28 +0300

    Исправляет тест для review-form

--- project/src/components/review-form/review-form.test.js
+++ project/src/components/review-form/review-form.test.js
@@ -5,6 +5,7 @@ import * as redux from 'react-redux';
 import userEvent from '@testing-library/user-event';
 import ReviewForm from './review-form';
 import '@testing-library/jest-dom/extend-expect';
+import { ReviewData } from '../../common/const';
 
 const mockStore = configureStore();
 
@@ -17,11 +18,12 @@ it('Render \'ReviewForm\'', () => {
   );
 
   expect(screen.getByTestId('reviews-form')).toBeInTheDocument();
-  expect(screen.getByTestId('form-rating')).toBeInTheDocument();
 
   expect(screen.getByText('Submit')).toBeInTheDocument();
 
   userEvent.type(screen.getByTestId('reviews-textarea'), 'Test');
   expect(screen.getByDisplayValue('Test')).toBeInTheDocument();
+  expect(screen.getAllByTestId('stars-rating')).toHaveLength(ReviewData.MAX_RATING);
+  expect(screen.getAllByTestId('stars-rating-label')).toHaveLength(ReviewData.MAX_RATING);
 });
 
